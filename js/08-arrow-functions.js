e772a9d1f1c827bbe2d077ee91e732d01a208a15
vejuota <egle.nemickaite@gmail.com>
2022-04-04 11:10:33 +0300

    pirma raide

--- js/08-arrow-functions.js
+++ js/08-arrow-functions.js
@@ -2,23 +2,23 @@ const pirmas = 7;
 const antras = 5;
 
 //iprasta funkcija
-function suma(a,b) {
-    return a+b;
+function suma(a, b) {
+  return a + b;
 }
 
-const r1 = suma(pirmas,antras);
+const r1 = suma(pirmas, antras);
 console.log(r1);
 
 //kintamajam priskirta anonimine funkcija
 
-function atimtis (a,b) {
-return a - b;
+function atimtis(a, b) {
+  return a - b;
 }
-const r2 = atimtis(pirmas,antras)
+const r2 = atimtis(pirmas, antras);
 console.log(r2);
 
 //function dalyba (a,b) {
- /*   return a / b;
+/*   return a / b;
     }
     const r3 = dalyba(pirmas,antras)
    console.log(r3); */
@@ -28,17 +28,24 @@ console.log(r2);
 // jei logikos bloke {} yra tik viena procedura, tai galime nerasyti {} ir return
 //const daugyba = (a,b) => a*b;
 
-
-const daugyba = (a,b) => {
-    return a*b;
-}
-    const r4 = daugyba (pirmas,antras);
-    console.log(r4);
-
+const daugyba = (a, b) => {
+  return a * b;
+};
+const r4 = daugyba(pirmas, antras);
+console.log(r4);
 
 //jei parametru bloke yra tik 1 parametras, tai galime nerasyti ()
-const kvadratu  = a => a*a
-        const r5 = kvadratu  (pirmas);
-        console.log(r5);
+const kvadratu = (a) => a * a;
+const r5 = kvadratu(pirmas);
+console.log(r5);
 
+const tekstas = 'Labas rytas';
 
+/*function pirmaRaide(text) {
+    return text [0];
+}
+console.log(pirmaRaide(tekstas));
+*/
+
+const pirmaRaide = s => s[0];
+console.log(pirmaRaide(tekstas));
